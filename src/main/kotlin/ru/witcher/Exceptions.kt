df31e2b95c37f3002c339ad5bb86c63806018938
Alex Mihailov <av.mihailov.dev@gmail.com>
2022-05-10 15:09:16 +0300

    Примеры для показа перехвата исключений в корутинах

--- src/main/kotlin/ru/witcher/Exceptions.kt
+++ src/main/kotlin/ru/witcher/Exceptions.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.*
 fun main() {
     propagateException()
     whoMyParent()
+    dealingWithExceptions()
 }
 
 fun propagateException() {
@@ -78,3 +79,38 @@ fun whoMyParent() {
     }
     Thread.sleep(1000)
 }
+
+fun dealingWithExceptions() = runBlocking {
+    launch {
+        try {
+            // With launch, exceptions will be thrown as soon as they happen
+            throwException("Launch")
+        } catch (e: Exception) {
+            println("Catch exception $e")
+        }
+    }
+    // When async is used as a root coroutine, exceptions are thrown when you call .await
+    supervisorScope {
+        val deferred = async { throwException("Async") }
+        try {
+            deferred.await()
+        } catch (e: Exception) {
+            println("Catch exception $e")
+        }
+    }
+
+    launch {
+//        supervisorScope { async { throwException("Async in Launch") } }.await()
+        async { throwException("Async in Launch") }
+    }
+
+    // Exceptions thrown in a coroutineScope builder or in coroutines created by other coroutines won’t be caught in a try/catch!
+//    try {
+//        launch { throwException("Uncaught launch") }
+//        async { throwException("Uncaught async") }
+//    } catch (e: Exception) {
+//        println("Catch exception $e")
+//    }
+}
+
+fun throwException(msg: String): Unit = throw RuntimeException(msg)
